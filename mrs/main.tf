553746f52df0a7219de38e65c79f6ab95f297e19
Carlos Ramirez <carlosrv125@icloud.com>
2020-03-18 07:22:32 +0800

    add rds and mrs, test

---
+++ mrs/main.tf
@@ -0,0 +1,68 @@
+# Configure the HuaweiCloud Provider with AK/SK
+# This will work with a single defined/default network, otherwise you need to specify network
+# to fix errors about multiple networks found.
+provider "huaweicloud" {
+  tenant_name = "${var.region}"
+  region      = "${var.region}"
+  access_key  = "${var.ak}"
+  secret_key  = "${var.sk}"
+  # the auth url format follows: https://iam.{region_id}.myhwclouds.com:443/v3
+  auth_url    = "https://iam.${var.region}.myhuaweicloud.com/v3"
+}
+
+# Create a VPC, Network and Subnet
+resource "huaweicloud_vpc_v1" "vpc_v1" {
+  name = "terraform"
+  cidr = "192.168.0.0/16"
+}
+
+resource "huaweicloud_vpc_subnet_v1" "subnet_v1" {
+  name              = "sb-terraform"
+  cidr              = "192.168.0.0/24"
+  gateway_ip        = "192.168.0.1"
+  vpc_id            = "${huaweicloud_vpc_v1.vpc_v1.id}"
+  dns_list          = ["100.125.1.250","8.8.8.8"]
+  availability_zone = "la-south-2a"
+}
+
+resource "huaweicloud_mrs_cluster_v1" "cluster1" {
+  cluster_name          = "mrs-cluster"
+  region                = "la-south-2"
+  billing_type          = 12
+  master_node_num       = 2
+  core_node_num         = 3
+  master_node_size      = "c3.xlarge.2.linux.bigdata"
+  core_node_size        = "c3.xlarge.2.linux.bigdata"
+  available_zone_id     = "la-south-2a"
+  vpc_id                = "${huaweicloud_vpc_v1.vpc_v1.id}"
+  subnet_id             = "${huaweicloud_vpc_subnet_v1.subnet_v1.id}"
+  cluster_version       = "MRS 1.8.7"
+  volume_type           = "SATA"
+  volume_size           = 100
+  safe_mode             = 0
+  cluster_type          = 0
+  node_public_cert_name = "KeyPair-TF"
+  cluster_admin_secret  = ""
+  component_list {
+    component_name = "Hadoop"
+  }
+  component_list {
+    component_name = "Spark"
+  }
+  component_list {
+    component_name = "Hive"
+  }
+}
+
+# Variables
+variable "ak" {
+  type = string
+}
+
+variable "sk" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
--- rds/main.tf
+++ rds/main.tf
@@ -12,7 +12,7 @@ provider "huaweicloud" {
 
 # Create a VPC, Network and Subnet
 resource "huaweicloud_vpc_v1" "vpc_v1" {
-  name = "vpc-tftest"
+  name = "test1"
   cidr = "192.168.0.0/16"
 }
 
@@ -27,7 +27,7 @@ resource "huaweicloud_vpc_subnet_v1" "subnet_v1" {
 
 # Create Security Group and rule ssh
 resource "huaweicloud_networking_secgroup_v2" "secgroup_1" {
-  name        = "secgroup_1"
+  name        = "secgroup_test"
   description = "My neutron security group"
 }
 
@@ -35,45 +35,29 @@ resource "huaweicloud_networking_secgroup_rule_v2" "secgroup_rule_1" {
   direction         = "ingress"
   ethertype         = "IPv4"
   protocol          = "tcp"
-  port_range_min    = 22
-  port_range_max    = 22
+  port_range_min    = 5432
+  port_range_max    = 5432
   remote_ip_prefix  = "0.0.0.0/0"
   security_group_id = "${huaweicloud_networking_secgroup_v2.secgroup_1.id}"
 }
 
-resource "huaweicloud_cce_cluster_v3" "cluster_1" {
-  name                   = "cluster-test"
-  billing_mode           = 0
-  cluster_type           = "VirtualMachine"
-  cluster_version        = "v1.13.7-r0"
-  flavor_id              = "cce.s1.small"
-  vpc_id                 = "${huaweicloud_vpc_v1.vpc_v1.id}"
-  subnet_id              = "${huaweicloud_vpc_subnet_v1.subnet_v1.id}"
-  container_network_type = "overlay_l2"
-  authentication_mode    = "rbac"
-  container_network_cidr = "172.16.0.0/16"
-}
-
-resource "huaweicloud_cce_node_v3" "node_1" {
-  cluster_id        = "${huaweicloud_cce_cluster_v3.cluster_1.id}"
-  name              = "node1"
-  flavor_id         = "s3.large.2"
-  iptype            = "5_bgp"
-  availability_zone = "${huaweicloud_vpc_subnet_v1.subnet_v1.availability_zone}"
-  key_pair          = "KeyPair-TF"
-  
-  root_volume {
-    size            = 40
-    volumetype      = "SATA"
+resource "huaweicloud_rds_instance_v3" "instance" {
+  availability_zone = ["la-south-2a"]
+  db {
+    password = "Huangwei#120521"
+    type = "PostgreSQL"
+    version = "9.5"
+    port = "5432"
   }
-  
-  sharetype         = "PER"
-  bandwidth_size    = 100
-  
-  data_volumes {
-    size            = 100
-    volumetype      = "SATA"
+  name = "terraform_test_rds_instance"
+  security_group_id = "${huaweicloud_networking_secgroup_v2.secgroup_1.id}"
+  subnet_id = "${huaweicloud_vpc_subnet_v1.subnet_v1.id}"
+  vpc_id = "${huaweicloud_vpc_v1.vpc_v1.id}"
+  volume {
+    type = "ULTRAHIGH"
+    size = 100
   }
+  flavor = "rds.pg.c2.large"
 }
 
 # Variables
