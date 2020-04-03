073c4622ce25ce6a929f9b812a5aad61f87c76d9
Carlos Ramirez <carlosrv125@icloud.com>
2020-04-03 13:45:00 -0500

    refactor, add AZ ID

--- mrs/main.tf
+++ mrs/main.tf
@@ -2,10 +2,10 @@
 # This will work with a single defined/default network, otherwise you need to specify network
 # to fix errors about multiple networks found.
 provider "huaweicloud" {
-  tenant_name = "${var.region}"
-  region      = "${var.region}"
-  access_key  = "${var.ak}"
-  secret_key  = "${var.sk}"
+  tenant_name = var.region
+  region      = var.region
+  access_key  = var.ak
+  secret_key  = var.sk
   # the auth url format follows: https://iam.{region_id}.myhwclouds.com:443/v3
   auth_url    = "https://iam.${var.region}.myhuaweicloud.com/v3"
 }
@@ -20,22 +20,22 @@ resource "huaweicloud_vpc_subnet_v1" "subnet_v1" {
   name              = "sb-terraform"
   cidr              = "192.168.0.0/24"
   gateway_ip        = "192.168.0.1"
-  vpc_id            = "${huaweicloud_vpc_v1.vpc_v1.id}"
+  vpc_id            = huaweicloud_vpc_v1.vpc_v1.id
   dns_list          = ["100.125.1.250","8.8.8.8"]
   availability_zone = "${var.region}a"
 }
 
 resource "huaweicloud_mrs_cluster_v1" "cluster1" {
   cluster_name          = "mrs-cluster"
-  region                = "${var.region}"
+  region                = var.region
   billing_type          = 12
   master_node_num       = 2
   core_node_num         = 3
   master_node_size      = "c3.xlarge.2.linux.bigdata"
   core_node_size        = "c3.xlarge.2.linux.bigdata"
-  available_zone_id     = "${var.region}a"
-  vpc_id                = "${huaweicloud_vpc_v1.vpc_v1.id}"
-  subnet_id             = "${huaweicloud_vpc_subnet_v1.subnet_v1.id}"
+  available_zone_id     = "4a5642bf1d7e421a8db631a447e140bc"
+  vpc_id                = huaweicloud_vpc_v1.vpc_v1.id
+  subnet_id             = huaweicloud_vpc_subnet_v1.subnet_v1.id
   cluster_version       = "MRS 1.8.7"
   volume_type           = "SATA"
   volume_size           = 100
